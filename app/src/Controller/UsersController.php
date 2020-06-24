7376da8e507cb312ac7c596241095ff6f36c06b1
tkzw_yuki <gulgul.groove@gmail.com>
2020-06-24 22:24:16 +0900

    ユーザ更新機能を実装

--- app/src/Controller/UsersController.php
+++ app/src/Controller/UsersController.php
@@ -38,4 +38,50 @@ class UsersController extends AppController
         }
         $this->set(compact('user'));
     }
+
+    /**
+     * ユーザー編集画面/ユーザー情報更新処理
+     *
+     * @return void
+     */
+    public function edit()
+    {
+        $user = $this->Users->get($this->Auth->user('id'));
+        if ($this->request->is('put')) {
+            $user = $this->Users->pathEntity($user, $this->request->getData());
+            if ($this->Users->save($user)) {
+                $this->Auth->setUser($user->toArray());
+
+                $this->Flash->success('ユーザー情報を更新しました');
+
+                return $this->redirect(['controller' => 'Questions', 'action' => 'index']);
+            }
+            $this->Flash->error('ユーザー情報の更新に失敗しました');
+        }
+        $this->set(compact('user'));
+    }
+
+    /**
+     * パスワード更新画面/パスワード更新処理
+     *
+     * @return \Cake\Http\Response|null パスワード更新後にユーザー編集画面へ遷移する
+     */
+    public function password()
+    {
+        $user = $this->Users->newEntity();
+        if ($this->request->is('post')) {
+            $user = $this->Users->get($this->Auth->user('id'));
+
+            $user = $this->Users->patchEntity($user, $this->request->getData());
+            if ($this->Users->save($user)) {
+                $this->Auth->setUser($user->toArray());
+
+                $this->Flash->success('パスワードを更新しました');
+
+                return $this->redirect(['action' => 'edit']);
+            }
+            $this->Flash->error('パスワードの更新に失敗しました');
+        }
+        $this->set(compact('user'));
+    }
 }
--- app/src/Model/Table/UsersTable.php
+++ app/src/Model/Table/UsersTable.php
@@ -3,6 +3,8 @@
 namespace App\Model\Table;
 
 use Cake\ORM\Table;
+use Cake\Auth\DefaultPasswordHasher;
+use Cake\Validation\Validator;
 
 /**
  * Users Model
@@ -24,6 +26,71 @@ class UsersTable extends Table
         $this->addBehavior('Timestamp');
     }
 
-}
+    /**
+     * バリデーションルールの定義
+     *
+     * @param \Cake\Validation\Validator $validator バリデーションインスタンス
+     * @return \Cake\Validation\Validator バリデーションインスタンス
+     */
+    public function validationDefault(Validator $validator)
+    {
+        $validator
+            ->nonNegativeInteger('id', 'IDが不正です')
+            ->allowEmpty('id', 'create', 'IDが不正です');
 
-?>
+        $validator
+            ->requirePresence('username', 'create', 'ユーザー名が不正です')
+            ->notEmpty('username', 'ユーザー名は必ず入力してください')
+            ->maxLength('username', 10, 'ユーザー名は10文字以内で入力してください')
+            ->add('username', 'alphaNumeric', [
+                'rule' => function ($value) {
+                    $pattern = '/\A[a-zA-Z0-9]+\z/';
+                    return (bool) preg_match($pattern, $value);
+                },
+                'message' => 'ユーザー名は半角英数字のみ入力してください'
+            ])
+            ->add('username', 'unique', [
+                'rule' => 'validateUnique',
+                'provider' => 'table',
+                'message' => 'そのユーザー名は既に使用されています'
+            ]);
+
+        $validator
+            ->scalar('nickname', 'ニックネームが不正です')
+            ->requirePresence('nickname', 'create', 'ニックネームが不正です')
+            ->notEmpty('nickname', 'ニックネームは必ず入力してください')
+            ->maxLength('nickname', 20, 'ニックネームは20文字以内で入力してください');
+
+        $validator
+            ->requirePresence('password', 'create', 'パスワードが不正です')
+            ->notEmpty('password', 'ニックネームは必ず入力してください')
+            ->maxLength('password', [8, 16], 'パスワードは８文字以上１６文字以内で入力してください')
+            ->add('password', 'securePassword', [
+                'rule' => function ($value) {
+                    $pattern = '/\A([a-zA-Z]+(?=[0-9])|[0-9]+(?=[a-zA-Z]))[0-9a-zA-Z]+\z/';
+                    return (bool) preg_match($pattern, $value);
+                },
+                'message' => 'パスワードは半角英数字混在で入力してください'
+            ])
+            ->add('password', [
+                'compare' => [
+                    'rule' => ['compareWith', 'password_confirm'],
+                    'message' => '確認用のパスワードと一致しません'
+                ]
+            ]);
+
+        $validator
+            ->add('password_current', 'click', [
+                'rule' => function ($value, $context) {
+                    $user = $this->get($context['data']['id']);
+                    if ((new DefaultPasswordHasher)->check($value, $user->password)) {
+                        return true;
+                    }
+                    return false;
+                },
+                'message' => '現在のパスワードが正しくありません'
+            ]);
+
+        return $validator;
+    }
+}
